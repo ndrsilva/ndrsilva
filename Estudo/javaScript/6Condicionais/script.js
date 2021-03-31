11cbe8c56fa3acdcf001f2a755dcf444b07f0fb0
André Luís Silva Souza <andreluis.9097@gmail.com>
2021-03-31 10:55:33 -0300

    Condição

---
+++ Estudo/javaScript/6Condicionais/script.js
@@ -0,0 +1,13 @@
+var hora = 4
+if (hora < 6 ) {
+    console.log("Boa Madrugada!")
+    
+} else if (hora < 12 ) {
+    console.log("Bom Dia!")
+
+} else if (hora < 18) {
+    console.log("Boa Tarde!")
+
+} else if (hora <= 23) {
+    console.log("Boa Noite!")
+} 
