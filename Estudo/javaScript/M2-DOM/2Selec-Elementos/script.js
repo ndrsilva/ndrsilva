3e7ce6c644d0dd21c86d77a8a9490744d73a62ec
André Luís Silva Souza <andreluis.9097@gmail.com>
2021-04-04 00:55:12 -0300

    getElements JS

---
+++ Estudo/javaScript/M2-DOM/2Selec-Elementos/script.js
@@ -0,0 +1,7 @@
+document.getElementById('exemplo').innerHTML = "ndr";
+
+document.getElementsByClassName('lista')[0].innerHTML = "Batata, Tomate, Cenoura";
+
+document.getElementsByTagName('');
+
+document.getElementsByName('');
