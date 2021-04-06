d83a3a2f82f1d780cf665a008c8d69023c32df68
André Luís Silva Souza <andreluis.9097@gmail.com>
2021-04-06 00:46:52 -0300

    JS - Manipulando Class do elemento 2

--- Estudo/javaScript/M2-DOM/5Manipulando-Class-do-elemento 1/script.js
+++ Estudo/javaScript/M2-DOM/5Manipulando-Class-do-elemento 1/script.js
@@ -1,3 +1,4 @@
+/*
 function verde() {
     document.querySelector('#exemplo').classList.remove('vermelho')
     document.querySelector('#exemplo').classList.remove('azul')
@@ -17,4 +18,17 @@ function azul() {
     document.querySelector('#exemplo').classList.remove('verde')
     document.querySelector('#exemplo').classList.add('azul')
 
+}
+
+*/
+
+//Comente esse código e tire o de cima.
+function trocar() {
+    if(document.querySelector('#exemplo').classList.contains('verde')) {
+        document.querySelector('#exemplo').classList.remove('verde');
+        document.querySelector('#exemplo').classList.add('vermelho');
+    } else {
+        document.querySelector('#exemplo').classList.remove('vermelho');
+        document.querySelector('#exemplo').classList.add('verde');
+    }
 }
