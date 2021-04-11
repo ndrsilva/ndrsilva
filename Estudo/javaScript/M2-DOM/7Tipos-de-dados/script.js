cd22b04552144515a51e5a8ea123d73643dd1efb
André Luís Silva Souza <andreluis.9097@gmail.com>
2021-04-10 22:59:13 -0300

    JS - Tipos de dados

---
+++ Estudo/javaScript/M2-DOM/7Tipos-de-dados/script.js
@@ -0,0 +1,11 @@
+var nome = "ndr";
+var idade = 28;
+var salvo = true;
+var cidade;
+
+//typeof + variável - Serve para ver o tipo m da variável no console.
+
+var tipo = typeof idade;
+
+console.log(tipo);
+//alert(tipo);
